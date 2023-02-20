3034575d881fd148804c4163f9b304c5c773b842
Federico Navós <fedenavos@gmail.com>
2023-02-19 23:40:47 -0300

    Refactor and remove unused code

--- components/Message.tsx
+++ components/Message.tsx
@@ -1,60 +1,35 @@
 import React, { ReactNode, useEffect, useState } from "react";
 
 type MessageProps = {
-    content?: string | ReactNode;
+  content?: string | ReactNode;
 };
 
 export default function Message({ content }: MessageProps) {
   const [output, setOutput] = useState<string | ReactNode>("");
-  const cont = content as any;
-    
 
-    useEffect(() => {
-        if (typeof content === "string") {
-            const interval = setInterval(() => {
-                setOutput((prevOutput) => {
-                    const currentIndex = (prevOutput as string).length;
-                    if (currentIndex >= (content as string).length) {
-                        clearInterval(interval);
-                        return prevOutput;
-                    }
-                    const nextChar = (content as string)[currentIndex];
-                    return prevOutput + nextChar;
-                });
-            }, 25);
-            return () => clearInterval(interval);
-        } else {
-            setOutput(content);
-        }
-    }, [content]);
-
-    const renderElement = (element: any) => {
-        if (typeof element === "string") {
-            return element;
-        } else {
-            switch (element.type) {
-                case "span":
-                    return (
-                        <span>
-                            {element.props.children}
-                        </span>
-                    );
-                case "a":
-                    return (
-                        <a href={element.props.href} target="_blank" rel="noreferrer">
-                            {element.props.children}
-                        </a>
-                    );
-                default:
-                    return element;
-            }
-        }
-    };
+  useEffect(() => {
+    if (typeof content === "string") {
+      const interval = setInterval(() => {
+        setOutput((prevOutput) => {
+          const currentIndex = (prevOutput as string).length;
+          if (currentIndex >= content.length) {
+            clearInterval(interval);
+            return prevOutput;
+          }
+          const nextChar = content[currentIndex];
+          return prevOutput + nextChar;
+        });
+      }, 25);
+      return () => clearInterval(interval);
+    } else {
+        setOutput(content);
+    }
+  }, [content]);
 
   return (
     <div>
-        {content === "" && <span className="cursor">|</span>}
-        { output }
+      {output === "" && <span className="cursor">|</span>}
+      { output }
     </div>
   );
 }
